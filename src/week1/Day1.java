a4920be4fec0850d52fbd8ad2c97836f2b77eba7
pauled <drpauled@gmail.com>
2023-02-01 14:23:34 -0500

    Updated the pom file for Java version 19 correctly

---
+++ src/week1/Day1.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Day1 {
+    public static void main(String[] args) {//main
+        System.out.println("hello nurse!");//sout
+        int num=2;
+        double num2;
+        num2=4.5;
+        num=13;
+        String str="String me";
+        boolean bool=true;
+        System.out.println(str+bool);
+        num2=6/4.0;
+        System.out.println(num2);
+    }
+}
