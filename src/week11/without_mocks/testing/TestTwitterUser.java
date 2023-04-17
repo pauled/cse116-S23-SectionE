385855bae8b31de798809733f95af1089664585e
pauled <drpauled@gmail.com>
2023-04-17 06:55:21 -0400

    week11-examples

---
+++ src/week11/without_mocks/testing/TestTwitterUser.java
@@ -0,0 +1,13 @@
+package week11.without_mocks.testing;
+
+import org.junit.Test;
+
+public class TestTwitterUser {
+
+    @Test
+    public void testTwitterUserMoods(){
+        // How do we even test this??
+        // Run the code, then check Twitter..
+    }
+
+}
